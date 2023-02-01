b9881bc0eb04f4be0d381ca9f394f29f57688bc8
jojojo0501 <sarukiti0418@outlook.jp>
2023-02-01 22:36:07 +0900

    create project

---
+++ src/main/java/com/example/merikuri/MerikuriApplication.java
@@ -0,0 +1,13 @@
+package com.example.merikuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerikuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerikuriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/merikuri/MerikuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.merikuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerikuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
