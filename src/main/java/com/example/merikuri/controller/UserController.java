5423f0d85d0b0d5eb3eddd9c5c3b70deff3708dc
jojojo0501 <sarukiti0418@outlook.jp>
2023-02-17 22:24:45 +0900

    add comments

--- src/main/java/com/example/merikuri/controller/UserController.java
+++ src/main/java/com/example/merikuri/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.merikuri.controller;
 
 import com.example.merikuri.controller.param.UserFormParam;
+import com.example.merikuri.exception.ServerErrorException;
 import com.example.merikuri.model.CreateUserRequest;
 import com.example.merikuri.model.CreatedResponse;
 import com.example.merikuri.service.UserService;
@@ -8,6 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @AllArgsConstructor
@@ -23,6 +25,7 @@ public class UserController implements UsersApi {
                     userService.createUser(new UserFormParam(request))
             );
         } catch (Exception e) {
+            //TODO 例外処理（Handler実装）
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
