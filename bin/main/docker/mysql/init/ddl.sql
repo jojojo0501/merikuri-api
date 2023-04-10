f323a8ec6ec0417ab79a12a8590de321a60f3f3c
saishuhayato <hayato.saishu@rakus.partners.co.jp>
2023-04-10 21:20:27 +0900

    ddl作成

--- bin/main/docker/mysql/init/ddl.sql
+++ bin/main/docker/mysql/init/ddl.sql
@@ -13,6 +13,15 @@ CREATE TABLE users (
     delete_flg BOOLEAN NOT NULL DEFAULT 0
 );
 
+drop table if exists categories cascade;
+CREATE TABLE categories (
+    category_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    delete_flg BOOLEAN NOT NULL DEFAULT 0
+);
+
 drop table if exists products cascade;
 CREATE TABLE products (
     product_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -27,7 +36,10 @@ CREATE TABLE products (
     image_3 VARCHAR(255),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    delete_flg BOOLEAN NOT NULL DEFAULT 0
+    delete_flg BOOLEAN NOT NULL DEFAULT 0,
+    FOREIGN KEY (category_id) REFERENCES categories (category_id),
+    FOREIGN KEY (sale_user_id) REFERENCES users (user_id),
+    FOREIGN KEY (buy_user_id) REFERENCES users (user_id)
 );
 
 drop table if exists comments_boards cascade;
@@ -36,7 +48,8 @@ CREATE TABLE comments_boards (
     product_id BIGINT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    delete_flg BOOLEAN NOT NULL DEFAULT 0
+    delete_flg BOOLEAN NOT NULL DEFAULT 0,
+    FOREIGN KEY (product_id) REFERENCES products (product_id)
 );
 
 drop table if exists comments cascade;
@@ -47,25 +60,20 @@ CREATE TABLE comments (
     content VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    delete_flg BOOLEAN NOT NULL DEFAULT 0
-);
-
-drop table if exists categories cascade;
-CREATE TABLE categories (
-    category_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    delete_flg BOOLEAN NOT NULL DEFAULT 0
+    delete_flg BOOLEAN NOT NULL DEFAULT 0,
+    FOREIGN KEY (comment_board_id) REFERENCES comments_boards (comment_board_id)
 );
 
 drop table if exists favorites cascade;
 CREATE TABLE favorites (
+    favorite_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     product_id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    delete_flg BOOLEAN NOT NULL DEFAULT 0
+    delete_flg BOOLEAN NOT NULL DEFAULT 0,
+    FOREIGN KEY (product_id) REFERENCES products (product_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
 drop table if exists messages_boards cascade;
@@ -75,12 +83,14 @@ CREATE TABLE messages_boards (
     buy_user_id BIGINT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    delete_flg BOOLEAN NOT NULL DEFAULT 0
+    delete_flg BOOLEAN NOT NULL DEFAULT 0,
+    FOREIGN KEY (sale_user_id) REFERENCES users (user_id),
+    FOREIGN KEY (buy_user_id) REFERENCES users (user_id)
 );
 
 drop table if exists messages cascade;
 CREATE TABLE messages (
-    message_id BIGINT NO NULL PRIMARY KEY AUTO_INCREMENT,
+    message_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     message_board_id BIGINT NOT NULL,
     to_user_id BIGINT,
     from_user_id BIGINT,
@@ -88,7 +98,10 @@ CREATE TABLE messages (
     send_date TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    delete_flg BOOLEAN NOT NULL DEFAULT 0
+    delete_flg BOOLEAN NOT NULL DEFAULT 0,
+    FOREIGN KEY (message_board_id) REFERENCES messages_boards (message_board_id),
+    FOREIGN KEY (to_user_id) REFERENCES users (user_id),
+    FOREIGN KEY (from_user_id) REFERENCES users (user_id)
 );
 
 drop table if exists notice_titles cascade;
@@ -108,5 +121,6 @@ CREATE TABLE notice_contents (
     content VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    delete_flg BOOLEAN NOT NULL DEFAULT 0
+    delete_flg BOOLEAN NOT NULL DEFAULT 0,
+    FOREIGN KEY (notice_title_id) REFERENCES notice_titles (notice_title_id)
 );
